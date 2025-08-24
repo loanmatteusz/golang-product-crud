2c1c533e94ba856443c2d93d3b9305dadf2f8b69
loanmatteusz <loan.matteusz@gmail.com>
2025-08-24 15:49:53 -0300

    refactor: validate create product dto fields

--- backend/internal/dtos/product_dto.go
+++ backend/internal/dtos/product_dto.go
@@ -3,14 +3,14 @@ package dtos
 import "github.com/google/uuid"
 
 type CreateProductDTO struct {
-	Name       string     `json:"name" validate:"required"`
-	Price      int        `json:"price" validate:"required"`
+	Name       string     `json:"name" validate:"required,min=1"`
+	Price      int        `json:"price" validate:"required,gte=0"`
 	CategoryID *uuid.UUID `json:"category_id"`
 }
 
 type UpdateProductDTO struct {
-	Name       *string    `json:"name"`
-	Price      *int       `json:"price"`
+	Name       *string    `json:"name" validate:"min=1"`
+	Price      *int       `json:"price" validate:"gte=0"`
 	CategoryID *uuid.UUID `json:"category_id"`
 }
 
